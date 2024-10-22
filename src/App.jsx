bddde075958597b32a6d9427ed18f3a143fed3b8
adishjustine <156789207+adishjustine@users.noreply.github.com>
2024-10-23 03:04:45 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Virtual Mall</h1>
+      <p>This is your online platform for shops.</p>
+    </div>
+  );
+}
+
+export default App;
